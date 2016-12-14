443c7a86e0663d9f7e5d108b5c4a502b5bc29c1e
derek-young <dthomasy@gmail.com>
2016-12-14 14:48:54 -0800

    "Implement the functional pattern for queue"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var key = someInstance.generateKey(value);
+    storage[key] = value;
+  };
+
+  someInstance.generateKey = function(value) {
+    var now = new Date().getTime;
+    return now + value;
   };
 
   someInstance.dequeue = function() {
+    var firstIndex = Object.keys(storage)[0];
+    var temp = storage[firstIndex];
+    delete storage[firstIndex];
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
