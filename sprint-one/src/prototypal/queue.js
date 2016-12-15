81c747c80bef077e9cbd4a10eb0d61bf10c87046
derek-young <dthomasy@gmail.com>
2016-12-14 16:13:30 -0800

    "Update functional-shared to remove superfluous if statement and implement prototypal pattern for queue"

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.storage = {};
+  return newInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.size()] = value;
+  },
+  dequeue: function() {
+    var temp = this.storage[0];
+    for (var i = 1; i <= this.size(); i++) {
+      this.storage[i - 1] = this.storage[i];
+    }
+    delete this.storage[this.size() - 1];
+    return temp;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
