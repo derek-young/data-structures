d58c3b4f907284627be20dfc3e8e3dadd8b0d732
Derek Young <dthomasy@gmail.com>
2016-12-15 19:30:08 -0800

    Graph pattern utilizes a storage object for the nodes

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,49 +2,47 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this[node] = [];
+  this.storage[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  return node in this;
+  return node in this.storage;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  delete this[node];
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  return this[fromNode].indexOf(this[toNode]) > -1;
+  return this.storage[fromNode].indexOf(this.storage[toNode]) > -1;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this[fromNode].push(this[toNode]);
-  this[toNode].push(this[fromNode]);
+  this.storage[fromNode].push(this.storage[toNode]);
+  this.storage[toNode].push(this.storage[fromNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  var fromIndex = this[fromNode].indexOf(this[toNode]);
-  var toIndex = this[toNode].indexOf(this[fromNode]);
-  this[fromNode].splice(fromIndex, 1);
-  this[toNode].splice(toIndex, 1);
+  var fromIndex = this.storage[fromNode].indexOf(this.storage[toNode]);
+  var toIndex = this.storage[toNode].indexOf(this.storage[fromNode]);
+  this.storage[fromNode].splice(fromIndex, 1);
+  this.storage[toNode].splice(toIndex, 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for (var node in this) {
-    console.log(node);
-    if (Array.isArray(this[node])) {
-      cb(node);
-    }
+  for (var node in this.storage) {
+    cb(node);
   }
 };
 
